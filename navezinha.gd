e47977a265955abff80fc43d74fae6ad1c3574f4
Shimmer <leonardo_alvesilva2@hotmail.com>
2025-01-24 21:39:37 -0300

    y

--- navezinha.gd
+++ navezinha.gd
@@ -1,17 +1,13 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 600.0
 
 func _physics_process(delta: float) -> void:
-	var directionx := Input.get_axis("ui_left", "ui_right")
-	var directiony := Input.get_axis("ui_up","ui_down")
-	if directionx:
-		velocity.x = directionx * SPEED
+	var direction := Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
+	if direction:
+		velocity = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if directiony:
-		velocity.y = directiony * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity = Vector2.ZERO
+	print(velocity)
 
 	move_and_slide()
