d46a6acb385104a1c09a413c57439e955f9c5109
Shimmer <leonardo_alvesilva2@hotmail.com>
2025-01-27 12:39:14 -0300

    protótipo do spawner de bullet 100% completo

--- bala_prototipo.gd
+++ bala_prototipo.gd
@@ -5,11 +5,9 @@ class_name Bullet
 @export var bullet_life: float
 @export var direction: float
 enum TYPE {pequena_vermelha, pequena_azul, grande_roxa}
-var spawn_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn_position = position
 	await get_tree().create_timer(bullet_life).timeout
 	queue_free()
 
--- navezinha.gd
+++ navezinha.gd
@@ -1,22 +1,30 @@
 extends Area2D
+
+#determina a existência do sinal hit, usado pra conectar até o game-master eventualmente
 signal hit
 
+#velocidade do player
+@export_category("Opções nave")
+@export var SPEED = 600.0
 
-const SPEED = 600.0
-
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
+	#gera um vetor normalizado baseado no input, muito bom
 	var direction := Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
+	
+	#move o personagem quando tu tá apertando alguma direção
 	if direction:
+		#lembra de física? isso aqui é literal S = vt, pq velocidade é direção * rapidez, e delta é tempo
 		position += direction * SPEED * delta
+	
 
-	
-func handle_hit():
-	print("ai meu pancreas")
-	pass
-	
 func _on_area_entered(area: Area2D) -> void:
+	#se colidir com uma bala, é pra emitir o sinal hit
 	if(area.is_in_group("Bullets")):
-		handle_hit()
 		hit.emit()
+	#isso aqui vai virar um elif eventualmente pra implementar outras zonas
 	else:
 		print("ah yes, zona")
+
+#função interna que faz o efeito on_hit, usa isso aqui pra deixar invulnerável, MAS NÃO PRA VIDA, ISSO É O GAME MASTER QUEM FAZ
+func _on_hit() -> void:
+	print("ai meu pancreas")
